454474b649bb8f7f2fa126eb8b15b91f8a95bd0c
Rani Maharani <maharanilintang83@gmail.com>
2026-08-28 21:16:37 +0700

    myfiles

---
+++ app/my-files/page.tsx
@@ -0,0 +1,10 @@
+import MyFiles from '../components/MyFiles';
+import Sidebar from "../components/Sidebar"; 
+
+export default function MyFilesPage() {
+  return (
+    <div className="p-6">
+      <MyFiles />
+    </div>
+  );
+}
