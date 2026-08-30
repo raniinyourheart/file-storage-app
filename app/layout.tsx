2d21221d2ebd877b63605d0f4756dfb5fdb4568e
Rani Maharani <maharanilintang83@gmail.com>
2026-08-30 13:19:04 +0700

    kafka

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,15 +7,7 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
-export const metadata: Metadata = {
-  title: "Vaultly",
-  description: "File storage app",
-};
 
 export default function RootLayout({
   children,
